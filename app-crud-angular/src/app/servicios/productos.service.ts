92abd9b42adf5715e4a4626e85d2dd1dfc6a1136
kelly saldaña <kellysalb@hotmail.com>
2025-08-05 15:13:10 -0600

    organice un archivo

--- app-crud-angular/src/app/servicios/productos.service.ts
+++ app-crud-angular/src/app/servicios/productos.service.ts
@@ -4,13 +4,14 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProductosService {
-  private apiUrl = 'http://localhost/backend-php/api.php';
+   private apiUrl = 'http://localhost:8080/backend-php/api.php';
+;
 
 
   constructor(private http: HttpClient) {}
 
   getProductos(): Observable<any> {
-    return this.http.get('http://localhost/backend-php/api.php');
+    return this.http.get('http://localhost:8080/backend-php/api.php');
   }
 
   addProducto(producto: any): Observable<any> {
